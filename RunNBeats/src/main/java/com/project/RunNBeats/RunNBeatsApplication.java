46c7fbc26c2acb4ac10c03c575cd26450ab40593
Daria Cirstea <cirsteadaria88@gmail.com>
2025-03-15 23:17:16 +0200

    SpringBoot initializer project

---
+++ RunNBeats/src/main/java/com/project/RunNBeats/RunNBeatsApplication.java
@@ -0,0 +1,13 @@
+package com.project.RunNBeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunNBeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunNBeatsApplication.class, args);
+	}
+
+}
